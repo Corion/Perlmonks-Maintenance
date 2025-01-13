ad0087e945bd34384618875b25a4524572fae175
Max Maischein <corion@corion.net>
2025-01-13 12:56:45 +0100

    Suppress some warnings for empty statistics

--- scripts/perlmonks-server-check.pl
+++ scripts/perlmonks-server-check.pl
@@ -198,7 +198,7 @@ HTML
     for my $key (sort keys %server_status) {
         my ($url, $addr) = split /\0/, $key;
         print sprintf '<tr><td><a href="%s">%s</a></td><td>%s</td><td>%.2f s</td><td>%s</td></tr>',
-                                 $url, $url, $addr, $time_taken{ $key }, $server_status{ $key };
+                                 $url, $url, $addr, $time_taken{ $key } // '?', $server_status{ $key };
     }
 
     print <<HTML;
