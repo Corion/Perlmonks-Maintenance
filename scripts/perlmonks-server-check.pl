5c99f0eed39540a97d1dc1526c2f0c3f4b8d72c8
Max Maischein <corion@corion.net>
2025-01-13 12:52:47 +0100

    Cache unreachable hosts so we don't retry them for every URL

--- scripts/perlmonks-server-check.pl
+++ scripts/perlmonks-server-check.pl
@@ -28,10 +28,10 @@ GetOptions(
 $format //= 'text';
 
 my @ADDRS = qw/
-	perlmonks.org www.perlmonks.org css.perlmonks.org
-	perlmonks.com www.perlmonks.com css.perlmonks.com
-	perlmonks.net www.perlmonks.net css.perlmonks.net
-	/;
+        perlmonks.org www.perlmonks.org css.perlmonks.org
+        perlmonks.com www.perlmonks.com css.perlmonks.com
+        perlmonks.net www.perlmonks.net css.perlmonks.net
+        /;
 
 my $DNS = { # As of 2018-07-25:
   "css.perlmonks.com"      => ["216.92.34.251", "66.39.54.27"],
@@ -46,54 +46,54 @@ my $DNS = { # As of 2018-07-25:
   "www.perlmonks.org"      => ["216.92.34.251", "66.39.54.27"],
 };
 if (1) {
-	$DNS={};
-	my $resolver = new Net::DNS::Resolver(recurse => 1, debug => 0);
-	for my $addr (@ADDRS) {
-		# figure out the authoritative server
-		$resolver->nameservers('8.8.8.8');
-		my $packet = $resolver->send($addr, 'SOA');
-		my @server = map {$_->mname} grep {$_->type eq 'SOA'}
-			$packet->answer;
-		unless (@server==1)
-			{ warn "Didn't find exactly one SOA record for $addr"
-				." (@server)"; next }
-		$packet = $resolver->send($server[0], 'A');
-		my @nameservers = map {$_->address} grep {$_->type eq 'A'}
-			$packet->answer;
-		die "@server" unless @nameservers;
-		# query the authoritative server
-		$resolver->nameservers(@nameservers);
-		$packet = $resolver->send($addr, 'A');
-		my @ips = sort map {$_->address} grep {$_->type eq 'A'}
-			$packet->answer;
-		if( ! $quiet) {
-        		printf "%23s %-35s %s\n", $addr,
-        			"(\@$server[0]/@nameservers)", join ' ', @ips;
-		};
-		$DNS->{$addr} = \@ips;
-	}
-	#dd $DNS;
+        $DNS={};
+        my $resolver = new Net::DNS::Resolver(recurse => 1, debug => 0);
+        for my $addr (@ADDRS) {
+                # figure out the authoritative server
+                $resolver->nameservers('8.8.8.8');
+                my $packet = $resolver->send($addr, 'SOA');
+                my @server = map {$_->mname} grep {$_->type eq 'SOA'}
+                        $packet->answer;
+                unless (@server==1)
+                        { warn "Didn't find exactly one SOA record for $addr"
+                                ." (@server)"; next }
+                $packet = $resolver->send($server[0], 'A');
+                my @nameservers = map {$_->address} grep {$_->type eq 'A'}
+                        $packet->answer;
+                die "@server" unless @nameservers;
+                # query the authoritative server
+                $resolver->nameservers(@nameservers);
+                $packet = $resolver->send($addr, 'A');
+                my @ips = sort map {$_->address} grep {$_->type eq 'A'}
+                        $packet->answer;
+                if( ! $quiet) {
+                        printf "%23s %-35s %s\n", $addr,
+                                "(\@$server[0]/@nameservers)", join ' ', @ips;
+                };
+                $DNS->{$addr} = \@ips;
+        }
+        #dd $DNS;
 }
 
 our $force_peeraddr;
 around 'LWP::Protocol::http::_extra_sock_opts' => sub {
-	my $orig = shift;
-	die unless wantarray;
-	my @rv = $orig->(@_);
-	push @rv, PeerAddr => $force_peeraddr if defined $force_peeraddr;
-	return @rv;
+        my $orig = shift;
+        die unless wantarray;
+        my @rv = $orig->(@_);
+        push @rv, PeerAddr => $force_peeraddr if defined $force_peeraddr;
+        return @rv;
 };
 around 'LWP::Protocol::https::_get_sock_info' => sub {
-	my $orig = shift;
-	my ($self, $res, $sock) = @_;
-	my $cert = $sock->get_peer_certificate;
-	my @san = $cert->peer_certificate('subjectAltNames');
-	while (@san) {
-		my ($type_id, $value) = splice @san, 0, 2;
-		$res->push_header("Client-SSL-Cert-SubjectAltName"
-			=> "$type_id: $value");
-	}
-	$orig->(@_);
+        my $orig = shift;
+        my ($self, $res, $sock) = @_;
+        my $cert = $sock->get_peer_certificate;
+        my @san = $cert->peer_certificate('subjectAltNames');
+        while (@san) {
+                my ($type_id, $value) = splice @san, 0, 2;
+                $res->push_header("Client-SSL-Cert-SubjectAltName"
+                        => "$type_id: $value");
+        }
+        $orig->(@_);
 };
 
 our @params = ('node=Newest+Nodes', '');
@@ -111,48 +111,56 @@ sub status {
 my %certs;
 my %server_status;
 my %time_taken;
+my %unreachable;
 my $ua = LWP::UserAgent->new( ssl_opts => { verify_hostname => 0 }, timeout => 60 );
 for my $addr (sort keys %$DNS) {
     local $| = 1;
     for my $host (sort @{ $DNS->{$addr} }) {
-	status( "Requesting $addr from $host...");
+        status( "Requesting $addr from $host...");
         for my $path (@paths) {
             for my $param (@params) {
-		my $url = "https://$addr$path?$param";
-		my $key = "$url\0$host";
-	        status( "Requesting $url from $host...");
-		local $force_peeraddr = $host;
-		my $start_time = time;
-		my $res = $ua->get($url);
-		$time_taken{ $key } = time - $start_time;
-                if( ! $res->is_success ) {
-		    $server_status{ $key } = $res->status_line;
-		    warn "Host: $host: " . $res->status_line
-		        if ! $quiet;
-                } elsif( $res->content !~ /\bNODE\.title\b\s*=\s*([^\r\n]+)/ ) {
-                    my $title;
-                    if( $res->content =~ m!<title>\s*(.*?)\s*</title>!si ) {
-                        $title = $1;
-                    } else {
-                        $title = substr( $res->content, 100 );
-                    };
-		    $server_status{ $key } = "Didn't get a Perlmonks site from $host as $addr ('$title')";
-		    warn "Didn't get a Perlmonks site from $host as $addr ('$title')";
-                    #warn $res->content;
+                my $url = "https://$addr$path?$param";
+                my $key = "$url\0$host";
+                if( $unreachable{ $host }) {
+                    $server_status{ $key } = $unreachable{ $host };
                 } else {
-	            status( "Requested $url from $host ($1)");
-		    $server_status{ $key } = 'OK';
-		    my @peer = $res->header("client-peer");
-		    die "@peer" unless @peer==1 && $peer[0] eq "$host:443";
-		    my @issuer = $res->header("client-ssl-cert-issuer");
-		    my @subject = $res->header("client-ssl-cert-subject");
-		    my @san = $res->header("client-ssl-cert-subjectaltname");
-		    my $certstr = "Issuer: @issuer\nSubject: @subject\n"
-		      ."Subject Alt Names: @san\n";
-		    $certs{$certstr}->{"$host $addr"} = 1;
-                };
-            }
-	}
+                    status( "Requesting $url from $host...");
+                    local $force_peeraddr = $host;
+                    my $start_time = time;
+                    my $res = $ua->get($url);
+                    $time_taken{ $key } = time - $start_time;
+                    if( ! $res->is_success ) {
+                        $server_status{ $key } = $res->status_line;
+                        warn "Host: $host: " . $res->status_line
+                            if ! $quiet;
+                        if( $res->status_line =~ /Can't connect to /i ) {
+                            $unreachable{ $host } = $res->status_line;
+                        };
+                    } elsif( $res->content !~ /\bNODE\.title\b\s*=\s*([^\r\n]+)/ ) {
+                        my $title;
+                        if( $res->content =~ m!<title>\s*(.*?)\s*</title>!si ) {
+                            $title = $1;
+                        } else {
+                            $title = substr( $res->content, 100 );
+                        };
+                        $server_status{ $key } = "Didn't get a Perlmonks site from $host as $addr ('$title')";
+                        warn "Didn't get a Perlmonks site from $host as $addr ('$title')";
+                        #warn $res->content;
+                    } else {
+                        status( "Requested $url from $host ($1)");
+                        $server_status{ $key } = 'OK';
+                        my @peer = $res->header("client-peer");
+                        die "@peer" unless @peer==1 && $peer[0] eq "$host:443";
+                        my @issuer = $res->header("client-ssl-cert-issuer");
+                        my @subject = $res->header("client-ssl-cert-subject");
+                        my @san = $res->header("client-ssl-cert-subjectaltname");
+                        my $certstr = "Issuer: @issuer\nSubject: @subject\n"
+                          ."Subject Alt Names: @san\n";
+                        $certs{$certstr}->{"$host $addr"} = 1;
+                    };
+                }
+	    }
+        }
     }
 }
 status( "" );
@@ -165,12 +173,12 @@ if( $format eq 'text' ) {
     for my $key (sort keys %server_status) {
         my ($url, $addr) = split /\0/, $key;
         print sprintf "%-64s - %-16s - %s (%.2fs)\n",
-	              $url, $addr, $server_status{ $key }, $time_taken{$key};
+                      $url, $addr, $server_status{ $key }, $time_taken{$key};
     }
 
     for my $cert (sort keys %certs) {
-    	print "##### Certificate #####\n", $cert, "### Served by:\n";
-    	printf "%15s %s\n", @$_ for map {[split]} sort keys $certs{$cert}->%*;
+        print "##### Certificate #####\n", $cert, "### Served by:\n";
+        printf "%15s %s\n", @$_ for map {[split]} sort keys $certs{$cert}->%*;
     }
 } elsif( $format eq 'html' ) {
     my $updated = strftime '%Y-%m-%dT%H:%M:%SZ', gmtime;
@@ -190,7 +198,7 @@ HTML
     for my $key (sort keys %server_status) {
         my ($url, $addr) = split /\0/, $key;
         print sprintf '<tr><td><a href="%s">%s</a></td><td>%s</td><td>%.2f s</td><td>%s</td></tr>',
-	                         $url, $url, $addr, $time_taken{ $key }, $server_status{ $key };
+                                 $url, $url, $addr, $time_taken{ $key }, $server_status{ $key };
     }
 
     print <<HTML;
